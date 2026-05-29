8215fb0db02f0a4cd69cd3a36837a0787dfac411
Jl Sut Pi <sutpi070@gmail.com>
2026-05-29 15:40:29 +0700

    feat(auth): create AuthContext and provider for global state

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
+import { AuthContextProvider } from "./context/AuthProvider.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+);
